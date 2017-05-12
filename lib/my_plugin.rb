69619c0db41c3e92afd6f604cf373381a757d7f8
Takafumi Yamano <takafumiyamano@gmail.com>
2017-05-12 11:43:16 +0900

    add exclamatize helper

--- lib/my_plugin.rb
+++ lib/my_plugin.rb
@@ -1,3 +1,10 @@
+require 'my_plugin/version'
+
+if defined?(Rails::Railtie)
+  require 'my_plugin/railtie'
+else
+  raise 'my_plugin is not compatible with Rails 2.* or older'
+end
+
 module MyPlugin
-  # Your code goes here...
 end
---
+++ lib/my_plugin/view_helpers/action_view.rb
@@ -0,0 +1,11 @@
+module MyPlugin
+  module ViewHelpers
+    module ActionView
+      def exclamatize(string, number = 1)
+        string + '!' * number
+      end
+
+      ::ActionView::Base.send :include, self
+    end
+  end
+end
